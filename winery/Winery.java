dc1e8981844d371a57c37e03144bfd2d7b8972bf
gjorgjinatashevska <108901056+gjorgjinatashevska@users.noreply.github.com>
2023-11-07 17:40:50 +0100

    Initial commit.

--- winery/Winery.java
+++ winery/Winery.java
@@ -4,5 +4,6 @@ public class Winery {
     public static void main(String[] args) {
         System.out.println("Winery");
         System.out.println("Gjorgina");
+        System.out.println("Napravete site po barem eden commit :))))");
     }
 }
