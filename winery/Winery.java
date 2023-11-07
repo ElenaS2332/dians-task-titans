90ee5f220054f7c593634f8276f21177afe027d6
Olgica Upcheva <olgica.upcheva@students.finki.ukim.mk>
2023-11-07 18:09:03 +0100

    Update Winery.java

--- winery/Winery.java
+++ winery/Winery.java
@@ -5,5 +5,6 @@ public class Winery {
         System.out.println("Winery");
         System.out.println("Gjorgina");
         System.out.println("Napravete site po barem eden commit :))))");
+        System.out.println("Olgica");
     }
 }
