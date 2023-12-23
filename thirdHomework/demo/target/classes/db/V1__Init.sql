e52718c19a3652b76b4c5d2969f2e8930eef46de
ElenaS2332 <elena.stamenkovska@students.finki.ukim.mk>
2023-12-23 23:05:16 +0100

    Deleted target from gitignore.

---
+++ thirdHomework/demo/target/classes/db/V1__Init.sql
@@ -0,0 +1,27 @@
+CREATE TABLE wineries (
+                          id SERIAL PRIMARY KEY,
+                          name VARCHAR(100),
+                          latitude DOUBLE PRECISION,
+                          longitude DOUBLE PRECISION,
+                          location VARCHAR(100),
+                          image_url VARCHAR(100),
+                          description VARCHAR(1000),
+                          image VARCHAR(255)
+);
+
+CREATE TABLE reviews (
+                         id SERIAL PRIMARY KEY,
+                         score INT,
+                         comment VARCHAR(255),
+                         winery_id BIGINT REFERENCES wineries(id)
+);
+
+CREATE TABLE users (
+                            id INT PRIMARY KEY ,
+                            uid INT UNIQUE,
+                            name VARCHAR(100),
+                            surname VARCHAR(100),
+                            password VARCHAR(100),
+                            username VARCHAR(255),
+                            fullname VARCHAR(255),
+);
