db5848a9c7abff2f2a7c717e0d706c12f7c8a1b0
ElenaS2332 <elena.stamenkovska@students.finki.ukim.mk>
2024-01-27 23:49:45 +0100

    Registration changed.

--- thirdHomework/demo/registration-service/src/main/java/com/example/registrationservice/RegistrationServiceApplication.java
+++ thirdHomework/demo/registration-service/src/main/java/com/example/registrationservice/RegistrationServiceApplication.java
@@ -4,6 +4,7 @@ import com.netflix.discovery.AbstractDiscoveryClientOptionalArgs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.http.EurekaClientHttpRequestFactorySupplier;
 import org.springframework.cloud.netflix.eureka.http.RestTemplateDiscoveryClientOptionalArgs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,11 +14,12 @@ import org.springframework.context.annotation.Configuration;
 public class RegistrationServiceApplication {
 
     @Configuration
-    static class EurekaClientConfig {
+    public class EurekaClientConfig {
 
         @Bean
-        public RestTemplateDiscoveryClientOptionalArgs discoveryClientOptionalArgs() {
-            return new RestTemplateDiscoveryClientOptionalArgs() {};
+        public RestTemplateDiscoveryClientOptionalArgs discoveryClientOptionalArgs(
+                EurekaClientHttpRequestFactorySupplier eurekaClientHttpRequestFactorySupplier) {
+            return new RestTemplateDiscoveryClientOptionalArgs(eurekaClientHttpRequestFactorySupplier);
         }
     }
 
