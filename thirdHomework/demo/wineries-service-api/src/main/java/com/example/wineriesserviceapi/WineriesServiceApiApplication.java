0dc6d3bcafd9819fc12580f8d0033e4d7a30a779
ElenaS2332 <elena.stamenkovska@students.finki.ukim.mk>
2024-01-28 00:00:35 +0100

    Changes in Wineries

--- thirdHomework/demo/wineries-service-api/src/main/java/com/example/wineriesserviceapi/WineriesServiceApiApplication.java
+++ thirdHomework/demo/wineries-service-api/src/main/java/com/example/wineriesserviceapi/WineriesServiceApiApplication.java
@@ -4,6 +4,8 @@ import com.netflix.discovery.AbstractDiscoveryClientOptionalArgs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.http.EurekaClientHttpRequestFactorySupplier;
+import org.springframework.cloud.netflix.eureka.http.RestTemplateDiscoveryClientOptionalArgs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,12 +14,14 @@ import org.springframework.context.annotation.Configuration;
 public class WineriesServiceApiApplication {
 
     @Configuration
-    static class EurekaClientConfig {
+    public class EurekaClientConfig {
         @Bean
-        public AbstractDiscoveryClientOptionalArgs<?> discoveryClientOptionalArgs() {
-            return new AbstractDiscoveryClientOptionalArgs<Object>() {};
+        public RestTemplateDiscoveryClientOptionalArgs discoveryClientOptionalArgs(
+                EurekaClientHttpRequestFactorySupplier eurekaClientHttpRequestFactorySupplier) {
+            return new RestTemplateDiscoveryClientOptionalArgs(eurekaClientHttpRequestFactorySupplier);
         }
     }
+
     public static void main(String[] args) {
         SpringApplication.run(WineriesServiceApiApplication.class, args);
     }
